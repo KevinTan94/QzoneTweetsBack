b9270f3d3e35a42fa1905b30b94b245b83aea2fa
KevinTan94 <tanzike94@gmail.com>
2026-06-08 02:06:48 +0800

    Update params.py

--- params.py
+++ params.py
@@ -1,4 +1,71 @@
-cookies = {
+cookies = {    'yyb_muid': '041AB137D21662743925A71DD34C63A8',
+    'Qs_lvt_323937': '1777890241',
+    'Qs_pv_323937': '2637958271193329000',
+    'RK': '+ogW3/9UZU',
+    'ptcz': '8e11dfd422462b7e997330f6c7ed6aea7c9db0943ccf6e3a88e8140b75532baa',
+    '_qpsvr_localtk': '0.3220432495008261',
+    'pgv_pvid': '8544217510',
+    'uin': 'o1510517987',
+    'skey': '@kBngCwM5f',
+    'p_uin': 'o1510517987',
+    'pt4_token': 'TEBdKsfZ00alv7ODlH3RlKM5VFIw69k0P*2aBeAlRAU_',
+    'p_skey': '0a7mjbcATsFBBxIlrFudnsIhVCHwnlEwnBkxma5RlWA_',
+    'media_p_uin': '1510517987',
+    'qz_screen': '1280x720',
+    '1510517987_todaycount': '0',
+    '1510517987_totalcount': '5256',
+    'QZ_FE_WEBP_SUPPORT': '1',
+    'domainid': '5',
+    '__Q_w_s_hat_seed': '1',
+    'pgv_info': 'ssid=s3947511718&pgvReferrer=',
+    'qzmusicplayer': 'qzone_player_1510517987_1780850978592',
+    'qqmusic_uin': '1510517987',
+    'qqmusic_key': '@kBngCwM5f',
+    'qqmusic_fromtag': '6',
+    'domain_id': '5',
+    '_qz_cdn': '_qzs.qq.com_qzs.qq.com',
+    '__Q_w_s__QZN_TodoMsgCnt': '1',
+    'media_p_skey': '9K-of1vKhjQeCktmTShPKeXpzzmkbVrJOSPgmnkI_A3XA9O5XdEYCT8qQhWuIje6yVRYaIHRQDVe_XrNhKwlcw',
+    'cpu_performance_v8': '37',
+}
+
+headers = {
+    'accept': '*/*',
+    'accept-language': 'zh-CN,zh;q=0.9',
+    # 'cookie': 'yyb_muid=041AB137D21662743925A71DD34C63A8; Qs_lvt_323937=1777890241; Qs_pv_323937=2637958271193329000; RK=+ogW3/9UZU; ptcz=8e11dfd422462b7e997330f6c7ed6aea7c9db0943ccf6e3a88e8140b75532baa; _qpsvr_localtk=0.3220432495008261; pgv_pvid=8544217510; uin=o1510517987; skey=@kBngCwM5f; p_uin=o1510517987; pt4_token=TEBdKsfZ00alv7ODlH3RlKM5VFIw69k0P*2aBeAlRAU_; p_skey=0a7mjbcATsFBBxIlrFudnsIhVCHwnlEwnBkxma5RlWA_; media_p_uin=1510517987; qz_screen=1280x720; 1510517987_todaycount=0; 1510517987_totalcount=5256; QZ_FE_WEBP_SUPPORT=1; domainid=5; __Q_w_s_hat_seed=1; pgv_info=ssid=s3947511718&pgvReferrer=; qzmusicplayer=qzone_player_1510517987_1780850978592; qqmusic_uin=1510517987; qqmusic_key=@kBngCwM5f; qqmusic_fromtag=6; domain_id=5; _qz_cdn=_qzs.qq.com_qzs.qq.com; __Q_w_s__QZN_TodoMsgCnt=1; media_p_skey=9K-of1vKhjQeCktmTShPKeXpzzmkbVrJOSPgmnkI_A3XA9O5XdEYCT8qQhWuIje6yVRYaIHRQDVe_XrNhKwlcw; cpu_performance_v8=37',
+    'priority': 'u=1, i',
+    'referer': 'https://user.qzone.qq.com/1510517987',
+    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132", "Google Chrome";v="132"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-origin',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36',
+}
+
+params = {
+    'uin': '1510517987',
+    'begin_time': '',
+    'end_time': '',
+    'getappnotification': '1',
+    'getnotifi': '1',
+    'has_get_key': '',
+    'offset': '10',
+    'set': '0',
+    'count': '10',
+    'useutf8': '1',
+    'outputhtmlfeed': '1',
+    'grz': '0.5045770806897445',
+    'scope': '1',
+    'g_tk': '1596724484',
+}
+
+response = requests.get(
+    'https://user.qzone.qq.com/proxy/domain/ic2.qzone.qq.com/cgi-bin/feeds/feeds2_html_pav_all',
+    params=params,
+    cookies=cookies,
+    headers=headers,
 }
 
 headers = {
